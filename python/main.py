d6b8eac57b275aa2a3c521b3e3b766677dae9009
kingpeterson <kingpetersonwang@gmail.com>
2021-09-24 13:43:59 +0800

    Cleanup code

--- python/main.py
+++ python/main.py
@@ -1,10 +1,7 @@
-from model import build_model
+from model import build_model, load_weight
 import cv2
 import argparse
-import json
 import numpy as np
-
-LAYERS_WITH_WEIGHT = ['conv2d', 'dense']
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "-input", help="input img", dest="input")
@@ -12,54 +9,14 @@ parser.add_argument("-i", "-input", help="input img", dest="input")
 args = parser.parse_args()
 img_dir = args.input
 
-def load_weight():
-    with open('../ai_tech_challenge/lenet.json') as json_file:
-        weights = json.load(json_file)
-        # print(weights)
-        # print(weights.keys())
-    json_file.close()
-    return weights.keys(), weights
-
-def convert_str_float(weight):
-    weights = weight.split(' ')
-    weights = np.array(weights).astype(np.float32)
-    return weights
-
-
 model = build_model()
-keys, weights = load_weight()
-
-# load weight
-for layer in model.layers:
-    name = layer.name.split('_')[0]
-    if name not in LAYERS_WITH_WEIGHT:
-        continue
-
-    number = layer.name.split('_')[-1] if '_' in layer.name else ''
-
-    if number == '':
-        weight_name = name + '_weight'
-        bias_name = name + '_bias'
-    else:
-        weight_name = name + '_' + number + '_weight'
-        bias_name = name + '_' + number + '_bias'
-    
-    # print(weight_name)
-    # print(bias_name)
-    weight = weights[weight_name]
-    weight = convert_str_float(weight)
-
-    bias = weights[bias_name]
-    bias = convert_str_float(bias)
-
-    layer.weight = weight
-    layer.bias = bias
-
 
 img = cv2.imread(img_dir, 0)
 img = np.expand_dims(img, axis = -1)
 img = np.expand_dims(img, axis = 0)
 prediction = list(model.predict(img)[0])
-print(prediction)
+print('Resulting vector:')
+for i, each in enumerate(prediction):
+    print('{}    {}'.format(i, round(float(each), 4)))
 predicted_number = prediction.index(max(prediction))
-print(predicted_number)
+print('\nClassification: {}'.format(predicted_number))
--- python/model.py
+++ python/model.py
@@ -1,11 +1,26 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+import json
+import numpy as np
+
+LAYERS_WITH_WEIGHT = ['conv2d', 'dense']
+
+def parse_weight():
+    with open('../ai_tech_challenge/lenet.json') as json_file:
+        weights = json.load(json_file)
+        # print(weights)
+        # print(weights.keys())
+    json_file.close()
+    return weights.keys(), weights
+
+def convert_str_float(weight):
+    weights = weight.split(' ')
+    weights = np.array(weights).astype(np.float32)
+    return weights
 
 def build_model():
-    inputs = tf.keras.Input(shape=(32,32,1), name='input')
     model = keras.Sequential()
-    # model.add(inputs)
     model.add(layers.Conv2D(6, kernel_size=(5, 5), strides=(1, 1), activation='tanh', input_shape=(32,32,1), padding='valid'))
     model.add(layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
     # conv2d_1
@@ -19,8 +34,46 @@ def build_model():
     # dense_2
     model.add(layers.Dense(10, activation='sigmoid'))
 
+    keys, weights = parse_weight()
+    model = load_weight(model, weights)
     model.summary()
 
-    # output = model(inputs)
 
+    # for layer in model.layers:
+    #     print(layer.weight)
+    #     print(layer.bias)
+    return model
+
+def load_weight(model, weights):
+    for layer in model.layers:
+        name = layer.name.split('_')[0]
+        if name not in LAYERS_WITH_WEIGHT:
+            continue
+
+        number = layer.name.split('_')[-1] if '_' in layer.name else ''
+
+        if number == '':
+            weight_name = name + '_weight'
+            bias_name = name + '_bias'
+        else:
+            weight_name = name + '_' + number + '_weight'
+            bias_name = name + '_' + number + '_bias'
+        
+        # print(weight_name)
+        # print(bias_name)
+        weight = weights[weight_name]
+        weight = convert_str_float(weight)
+
+        bias = weights[bias_name]
+        bias = convert_str_float(bias)
+        # print(bias)
+
+        # print(layer)
+        # print(weight.shape)
+
+        layer.weight = weight
+        layer.bias = bias
+        layer.bias_initializer = None
+        layer.trainable = False
+        
     return model
